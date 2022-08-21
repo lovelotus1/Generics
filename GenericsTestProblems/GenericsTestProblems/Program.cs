718d4bf6ebd410fa4e387d485c7e8255f9703a26
“Love <opera.prem@gmail.com>
2022-08-21 09:43:33 +0430

    Added Welcome Message

---
+++ GenericsTestProblems/GenericsTestProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsTestProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Generics Test Problems");
+        }
+    } 
+}
