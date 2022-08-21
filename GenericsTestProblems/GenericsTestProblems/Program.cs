d61587bec9c2c6bfbba4d5e12f5182ae854bdfc1
“Love <opera.prem@gmail.com>
2022-08-21 09:52:42 +0430

    ADDED: Generics Program To Find Maximum Of Three Integer

--- GenericsTestProblems/GenericsTestProblems/Program.cs
+++ GenericsTestProblems/GenericsTestProblems/Program.cs
@@ -7,6 +7,19 @@ namespace GenericsTestProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Generics Test Problems");
+
+            Console.WriteLine("Enter First Number");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Third Number");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nMaximum Of 3 Integer");
+            int output = MaxInteger.MaximumIntegerNumber(a, b, c);
+            Console.WriteLine(output);
         }
-    } 
+    }
 }
